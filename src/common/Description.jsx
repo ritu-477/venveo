c323d9ea367c36c809e21e20385eb4afbeca3956
ritu-477 <ritujoharbishnoi@gmail.com>
2024-12-20 11:06:27 +0530

    changes

--- src/common/Description.jsx
+++
@@ -1,7 +0,0 @@
-const Description = ({ text, classStyle }) => {
-    return (
-        <p className={`text-white font-light text-center sm:text-2xl text-custom-lg leading-7 sm:leading-custom-md font-maisonNeue ${classStyle}`}>{text}</p>
-    )
-}
-
-export default Description
--- src/common/Heading.jsx
+++
@@ -1,7 +0,0 @@
-const Heading = ({ classStyle, spanText, text, spanTwo }) => {
-    return (
-        <h2 className={`${classStyle} xl:text-5xl xl:leading-custom-7xl font-testSöhneSchmal font-normal md:text-4xl text-custom-base max-md:leading-8`}><span className="font-bold">{spanText}</span> {text} <span className="font-bold">{spanTwo}</span></h2>
-    )
-}
-
-export default Heading
