0b5c9464e65d36cb14bb2d90c1de4c156dae630e
ritu-477 <ritujoharbishnoi@gmail.com>
2024-12-23 21:22:40 +0530

    changes

--- src/components/DigitalMarketing.jsx
+++ src/components/DigitalMarketing.jsx
@@ -1,87 +1,87 @@
 import React, { useState } from 'react';
 import Heading from '../common/Heading';
 import Icon from '../utils/icons';
-import { DIGITAL_MARKETING_LIST } from '../utils/helper'
+import { DIGITAL_MARKETING_LIST } from '../utils/helper';
 
 const DigitalMarketing = () => {
   const [showAll, setShowAll] = useState(false);
 
   return (
     <div className="lg:py-[66px] md:py-12 py-6 relative">
-      <img className='absolute sm:block hidden sm:top-[17%] xl:top-[29%] w-full z-[-1]' src="/assets/images/svg/yellow-line.svg" alt="yellow-line" />
-      <img src="/assets/images/svg/mobile-yellow-line.svg" className='w-full absolute top-[32%] sm:hidden block' alt="yellow-line" />
-      <div className="container">
-          <p className="font-light sm:text-3xl sm:leading-custom-6xl text-2xl leading-custom-5xl text-center font-maisonLight pb-[3px]">
-            Problems we solve.
-          </p>
-          <Heading
-            text={`DIGITAL MARKETING FOR TODAY'S BUSINESS`}
-            classStyle={"text-center max-w-[487px] mx-auto"}
-          />
-          <p className="font-normal text-custom-lg leading-5 font-maisonMono sm:pt-[22px] pt-5 text-center max-w-[293px] sm:max-w-[641px] mx-auto">
-            We help companies scale their strategies across multiple channels to drive more revenue, more quickly, without cutting corners.
-          </p>
-          <div className="sm:pt-[83px] pt-[92px] max-w-[1072px] mx-auto">
-            <div className="flex flex-wrap lg:gap-[66px] gap-10 justify-center w-full">
-              {DIGITAL_MARKETING_LIST.map((obj, index) => (
-                <div
-                  key={index}
-                  className={`border-2 bg-white border-aqua-green w-full max-w-[503px] px-[30px] lg:pt-[65px] lg:pb-[54px] pt-[34px] pb-[36.25px] rounded-[25px] lg:h-[506px] ${!showAll && index > 0 ? 'hidden sm:block' : ''
-                    }`}
-                >
-                  <div className="lg:flex max-lg:flex-wrap gap-6">
-                    <Icon classStyle={"lg:size-[53px] size-[39.57px]"} iconName={obj.iconName} />
-                    <div className="flex flex-col max-lg:mt-3">
-                      <p className="font-maisonLight text-2xl leading-custom-5xl lg:text-3xl lg:leading-custom-6xl font-light lg:max-w-[346px]">
-                        {obj.title}
-                      </p>
-                      <p
-                        className={`font-normal text-custom-lg leading-5 font-maisonMono lg:max-w-[315px] ${index === 0 || index === 1 ? "sm:pt-[22px] pt-[15px]" : "sm:pt-[42px] pt-[15px]"
-                          }`}
-                      >
-                        {obj.description}
-                      </p>
-                      <ul className="list-disc md:pt-8 pl-5 pt-4">
-                        {obj.points.map((point, idx) => (
-                          <li key={idx} className="text-custom-lg leading-5 font-semibold font-MaisonNeue max-w-[285px]">
-                            {point}
-                          </li>
-                        ))}
-                      </ul>
-                      <button
-                        className={`font-semibold text-custom-lg leading-5 max-w-[164px] flex gap-[6px] items-center transition-all duration-500 group ${index === 0
-                            ? "sm:pt-[42px] pt-8"
-                            : index === 1
+      <img className="absolute sm:block hidden sm:top-[17%] xl:top-[29%] w-full z-[-1]" src="/assets/images/svg/yellow-line.svg" alt="yellow-line" />
+      <div className='px-4 mx-auto'>
+        <p className="font-light sm:text-3xl sm:leading-custom-6xl text-2xl leading-custom-5xl text-center font-maisonLight pb-[3px]">
+          Problems we solve.
+        </p>
+        <Heading
+          text={`DIGITAL MARKETING FOR TODAY'S BUSINESS`}
+          classStyle={"text-center max-w-[487px] mx-auto"}
+        />
+        <p className="font-normal text-custom-lg leading-5 font-maisonMono sm:pt-[22px] pt-5 text-center max-w-[293px] sm:max-w-[641px] mx-auto max-sm:pb-[11px]">
+          We help companies scale their strategies across multiple channels to drive more revenue, more quickly, without cutting corners.
+        </p>
+        </div>
+      <img src="/assets/images/svg/mobile-yellow-line.svg" className="w-full sm:hidden block" alt="yellow-line" />
+        <div className="container">
+        <div className="sm:pt-[83px] pt-11 max-w-[1072px] mx-auto">
+          <div className="flex flex-wrap lg:gap-[66px] gap-10 justify-center w-full">
+            {DIGITAL_MARKETING_LIST.map((obj, index) => (
+              <div
+                key={index}
+                className={`border-2 bg-white border-aqua-green w-full max-w-[503px] px-[30px] lg:pt-[65px] lg:pb-[54px] pt-[34px] pb-[36.25px] rounded-[25px] lg:h-[506px] ${!showAll && index > 0 ? "hidden sm:block" : ""
+                  }`}
+              >
+                <div className="lg:flex max-lg:flex-wrap gap-6">
+                  <Icon classStyle={"lg:size-[53px] size-[39.57px]"} iconName={obj.iconName} />
+                  <div className="flex flex-col max-lg:mt-3">
+                    <p className="font-maisonLight text-2xl leading-custom-5xl lg:text-3xl lg:leading-custom-6xl font-light lg:max-w-[346px]">
+                      {obj.title}
+                    </p>
+                    <p
+                      className={`font-normal text-custom-lg leading-5 font-maisonMono lg:max-w-[315px] ${index === 0 || index === 1 ? "sm:pt-[22px] pt-[15px]" : "sm:pt-[42px] pt-[15px]"
+                        }`}
+                    >
+                      {obj.description}
+                    </p>
+                    <ul className="list-disc md:pt-8 pl-5 pt-4">
+                      {obj.points.map((point, idx) => (
+                        <li key={idx} className="text-custom-lg leading-5 font-semibold font-MaisonNeue max-w-[285px]">
+                          {point}
+                        </li>
+                      ))}
+                    </ul>
+                    <button
+                      className={`font-semibold text-custom-lg leading-5 max-w-[164px] flex gap-[6px] items-center transition-all duration-500 group ${index === 0
+                          ? "sm:pt-[42px] pt-8"
+                          : index === 1
+                            ? "sm:pt-[24px] pt-8"
+                            : index === 3
                               ? "sm:pt-[24px] pt-8"
-                              : index === 3
-                                ? "sm:pt-[24px] pt-8"
-                                : "sm:pt-[42px] pt-8"
-                          }`}>
-                        {obj.buttonText}
-                        <Icon
-                          classStyle={
-                            "group-hover:translate-x-1 transition-all duration-500"
-                          }
-                          iconName={"arrowIcon"}
-                        />
-                      </button>
-
-                    </div>
+                              : "sm:pt-[42px] pt-8"
+                        }`}
+                    >
+                      {obj.buttonText}
+                      <Icon
+                        classStyle={
+                          "group-hover:translate-x-1 transition-all duration-500"
+                        }
+                        iconName={"arrowIcon"}
+                      />
+                    </button>
                   </div>
                 </div>
-              ))}
-            </div>
-            {!showAll && (
-              <div className="flex justify-center mt-12 sm: sm:hidden">
-                <button
-                  className="font-semibold text-custom-lg leading-custom-lg"
-                  onClick={() => setShowAll(true)}
-                >
-                  Show More Solutions <span className='text-custom-sm leading-custom-xs'>▼</span>
-                </button>
               </div>
-            )}
+            ))}
           </div>
+          <div className="justify-center mt-12 max-sm:flex hidden">
+            <button
+              className="font-semibold text-custom-lg leading-custom-lg"
+              onClick={() => setShowAll(!showAll)}
+            >
+              {showAll ? "Show Less Solutions ▲" : "Show More Solutions ▼"}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/SatisfiedClients.jsx
+++ src/components/SatisfiedClients.jsx
@@ -23,31 +23,31 @@ const SatisfiedClients = () => {
           />
         </div>
         <div className='xl:flex hidden justify-center gap-[60px] max-w-[1920px] mx-auto pt-[88px]'>
-          {SATISFIED_CLIENT_LIST.map((card, index) => (
+          {SATISFIED_CLIENT_LIST.map((data, index) => (
             <div key={index}>
               <div
-                className={`${card.bgClass} bg-cover bg-no-repeat bg-center p-[35px_29px_33px_29px] max-w-[440px] h-[500px]`}
+                className={`${data.bgClass} bg-cover bg-no-repeat bg-center p-[35px_29px_33px_29px] max-w-[440px] h-[500px]`}
               >
                 <div className="max-w-[382px] p-[38px_24px_31px_24px] bg-white rounded-[25px]">
-                  {card.title && (
+                  {data.title && (
                     <div className="-mt-[55px] -ms-1 rounded-full py-[9px] max-w-[165px] px-3 group hover:bg-black transition-all duration-500">
                       <p className="text-xs group-hover:text-white text-black leading-5 font-semibold font-MaisonNeue transition-all duration-500">
-                        {card.title}
+                        {data.title}
                       </p>
                     </div>
                   )}
                   <a href="/">
                     <img
-                      src={card.logo}
+                      src={data.logo}
                       alt="logo"
                       className={`${index === 0 ? 'max-w-[131px] pt-5' : index === SATISFIED_CLIENT_LIST.length - 1 ? 'max-w-[49px] pt-0' : 'max-w-[149.73px] pt-5'}`}
                     />
                   </a>
 
                   <p className="font-maisonLight font-light text-2xl leading-custom-5xl sm:text-3xl sm:leading-custom-6xl pt-6">
-                    {card.description}
+                    {data.description}
                   </p>
-                  <p className="text-custom-lg leading-5 font-normal font-maisonMono pt-6">{card.details}</p>
+                  <p className="text-custom-lg leading-5 font-normal font-maisonMono pt-6">{data.details}</p>
                   <button
                     className="font-semibold text-custom-lg leading-5 max-w-[97px] flex gap-[9px] items-center pt-[54px] transition-all duration-500 group"
                     
@@ -57,7 +57,7 @@ const SatisfiedClients = () => {
                   </button>
                 </div>
                 <div className="flex gap-[46px] md:gap-[62px] pt-[13px]">
-                  {card.stats.map((stat, i) => (
+                  {data.stats.map((stat, i) => (
                     <div key={i} className="flex flex-col">
                       <p className="font-bold font-testSöhneSchmal sm:text-custom-3xl leading-custom-8xl text-custom-2xl">
                         {stat.value}
@@ -81,30 +81,30 @@ const SatisfiedClients = () => {
               1280: { slidesPerView: 3 },
             }}
           >
-            {SATISFIED_CLIENT_LIST.map((card, index) => (
+            {SATISFIED_CLIENT_LIST.map((data, index) => (
               <SwiperSlide key={index}>
                 <div
-                  className={`${card.bgClass} bg-cover bg-no-repeat bg-center p-[35px_20px_33px_20px] max-w-[440px] h-[500px]`}
+                  className={`${data.bgClass} bg-cover bg-no-repeat bg-center p-[35px_20px_33px_20px] max-w-[440px] h-[500px]`}
                 >
                   <div className="max-w-[382px] p-[38px_20px_32px_22px] bg-white rounded-[25px]">
-                    {card.title && (
+                    {data.title && (
                       <div className="-mt-[55px] -ms-1 rounded-full py-[9px] max-w-[165px] px-3 group hover:bg-black transition-all duration-500">
                         <p className="text-xs group-hover:text-white text-black leading-5 font-semibold font-MaisonNeue transition-all duration-500">
-                          {card.title}
+                          {data.title}
                         </p>
                       </div>
                     )}
                     <a href="/">
                       <img
-                        src={card.logo}
+                        src={data.logo}
                         alt="logo"
                         className={`${index === 0 ? 'max-w-[131px] pt-5' : index === SATISFIED_CLIENT_LIST.length - 1 ? 'max-w-[49px] pt-0' : 'max-w-[149.73px] pt-5'}`}
                       />
                     </a>
                     <p className="font-maisonLight font-light text-2xl leading-custom-5xl pt-6">
-                      {card.description}
+                      {data.description}
                     </p>
-                    <p className="text-custom-lg leading-5 font-normal font-maisonMono pt-6">{card.details}</p>
+                    <p className="text-custom-lg leading-5 font-normal font-maisonMono pt-6">{data.details}</p>
                     <button
                       className="font-semibold text-custom-lg leading-5 max-w-[97px] flex gap-[9px] items-center pt-[54px] transition-all duration-500 group"
                     >
@@ -113,7 +113,7 @@ const SatisfiedClients = () => {
                     </button>
                   </div>
                   <div className="flex gap-[46px] pt-[13px]">
-                    {card.stats.map((stat, i) => (
+                    {data.stats.map((stat, i) => (
                       <div key={i} className="flex flex-col">
                         <p className="font-bold font-testSöhneSchmal leading-custom-8xl text-custom-2xl">
                           {stat.value}
